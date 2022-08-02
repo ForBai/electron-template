d4f52994b47809ab54f7aa46ae0eac010bb16a70
ForBai <thelars42@gmail.com>
2022-08-02 19:38:56 +0200

    init

---
+++ build/webpack.app.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const base = require("./webpack.base.config");
+
+module.exports = env => {
+  return merge(base(env), {
+    entry: {
+      main: "./src/main/main.js",
+      app: "./src/main/app.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "../app")
+    }
+  });
+};
---
+++ e2e/hello_world.e2e.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import testUtils from "./utils";
+
+describe("application launch", function() {
+  //beforeEach(testUtils.beforeEach);
+  //afterEach(testUtils.afterEach);
+
+ // it("shows hello world text on screen after launch", function() {
+
+ // });
+});
